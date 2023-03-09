3597c7e8ccae2f54a1ddc9cc5685608b0416884f
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-03-09 15:19:52 +0100

    sample application scope bean added

---
+++ src/main/java/com/piotrgrochowiecki/introtojsf/ApplicationScopeBean.java
@@ -0,0 +1,10 @@
+package com.piotrgrochowiecki.introtojsf;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+
+@Named("appScopedBean")
+@ApplicationScoped
+public class ApplicationScopeBean {
+
+}
