68caf7836fd00100cf70980c5e8ef3a74991e89f
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-03-16 14:36:03 +0100

    JSF convertes 2

--- src/main/java/com/piotrgrochowiecki/introtojsf/HTML5PageBean.java
+++ src/main/java/com/piotrgrochowiecki/introtojsf/HTML5PageBean.java
@@ -3,15 +3,36 @@ package com.piotrgrochowiecki.introtojsf;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 
+import java.util.Date;
+
 @RequestScoped
 @Named("html5bean")
 public class HTML5PageBean {
 
     private String dateIn;
+    private Date dateObjectIn;
+    private Date dateOnjectOut;
+
+    public Date getDateOnjectOut() {
+        return dateOnjectOut;
+    }
+
+    public void setDateOnjectOut(Date dateOnjectOut) {
+        this.dateOnjectOut = dateOnjectOut;
+    }
+
     private String dateOut;
 
+    public Date getDateObjectIn() {
+        return dateObjectIn;
+    }
+
+    public void setDateObjectIn(Date dateObjectIn) {
+        this.dateObjectIn = dateObjectIn;
+    }
+
     public void transformDates() {
-        dateOut = dateIn;
+        dateOnjectOut = dateObjectIn;
     }
 
     public String getDateIn() {
